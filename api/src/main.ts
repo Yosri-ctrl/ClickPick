3d4668f996e26b531c8fcf4a48fea56720242ac8
Yosri-ctrl <yosribouabid@gmail.com>
2023-02-02 22:15:19 +0100

    nestjs initialisation

---
+++ api/src/main.ts
@@ -0,0 +1,12 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger();
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.SM_MAIN_PORT || 3000;
+  await app.listen(port);
+  logger.log(`App running on port: ${port}`);
+}
+bootstrap();
