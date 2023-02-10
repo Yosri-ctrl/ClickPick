01fd90bf5fcb82dd42aa005d1fc05a11e1941fd5
Yosri-ctrl <yosribouabid@gmail.com>
2023-02-10 08:20:00 +0100

    commit message here

--- api/src/groups/groups.controller.ts
+++ api/src/groups/groups.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
@@ -17,7 +25,19 @@ export class GroupsController {
     @Body() createGroupDto: CreateGroupDto,
     @GetUser() user: User,
   ): Promise<Group> {
-    this.logger.verbose(`Creating group: ${createGroupDto.title}`);
+    this.logger.verbose(`Creating group: '${createGroupDto.title}'`);
     return this.groupService.createGroup(createGroupDto, user);
+  }
+
+  @Get('/getall')
+  getAllGroups(): Promise<Group[]> {
+    this.logger.verbose(`Getting all groups`);
+    return this.groupService.getAllGroups();
+  }
+
+  @Get('/:id')
+  getGroup(@Param('id') id: string): Promise<Group> {
+    this.logger.verbose(`Getting group: '${id}'`);
+    return this.groupService.getGroup(id);
   }
 }
--- api/src/groups/groups.service.ts
+++ api/src/groups/groups.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
 import { Repository } from 'typeorm';
@@ -22,12 +28,19 @@ export class GroupsService {
   ): Promise<Group> {
     const { title, description } = createGroupDto;
 
-    // title already exists
-    const group: Group = this.groupRepository.create({
-      title,
-      description,
-      type: groupTypes.public,
-    });
+    let group: Group = await this.groupRepository.findOneBy({ title });
+    if (group) {
+      this.logger.error(`Group with title: '${title}' already exists`, '');
+      throw new ConflictException(
+        `Group with title: '${title}' already exists`,
+      );
+    } else {
+      group = this.groupRepository.create({
+        title,
+        description,
+        type: groupTypes.public,
+      });
+    }
     await this.groupRepository.save(group);
 
     const role: GroupsRole = this.groupRoleRepository.create({
@@ -38,5 +51,26 @@ export class GroupsService {
     await this.groupRoleRepository.save(role);
 
     return group;
+  }
+
+  async getGroup(id: string): Promise<Group> {
+    const group: Group = await this.groupRepository.findOneBy({ id });
+    if (!group) {
+      this.logger.error(`Group: ${id} dosen't exists`);
+      throw new NotFoundException(`Group: ${id} dosen't exists`);
+    }
+    return group;
+  }
+
+  async getAllGroups(): Promise<Group[]> {
+    const quary = this.groupRepository.createQueryBuilder('group');
+    const groups = await quary.getMany();
+
+    if (!groups) {
+      this.logger.error(`Couldn't find groups`);
+      throw new UnauthorizedException();
+    }
+
+    return groups;
   }
 }
